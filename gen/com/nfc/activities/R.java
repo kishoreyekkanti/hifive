026ac185e8b2ec46d46035ab3dd3b3a62b7e4912
Kishore Kumar Yekkanti <kishoreyekkanti@yahoo.com>
2011-03-02 21:16:09 +0530

    Images added

--- gen/com/nfc/activities/R.java
+++ gen/com/nfc/activities/R.java
@@ -11,7 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int hifive=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int read_data=0x7f020002;
+        public static final int save_data=0x7f020003;
+        public static final int write_data=0x7f020004;
     }
     public static final class id {
         public static final int business_card_text=0x7f060003;
